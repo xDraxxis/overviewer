b9216c3171bcd95279298bfcdeef2db4548c44b8
ThatRumbu <45591775+ThatRumbu@users.noreply.github.com>
2019-08-31 18:25:37 +1000

    Spawn
    500 block radius centered at 0,0

--- README.md
+++
@@ -1,7 +0,0 @@
-# XMC Network Overviewer
-This repository will be used to store overviewer renders of XMC Network worlds.
-
-## Renders
-### XMC Vanilla
-- August 16th - Server launch
-- August 31st
---
+++ overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "icons/marker_home.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "queryMarker": "icons/marker_location.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "XMC Season 3"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1567237585",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "August 31st 2019",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "overworld_south",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "XMC Season 3",
+            "last_rendertime": 1567230257,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -61,
+                96,
+                -128
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -61,
+                96,
+                -128
+            ],
+            "north_direction": 0
+        }
+    ]
+};
